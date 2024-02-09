fd77b7dfa001c43ce543c5684242b7760b8a72a6
JstVex <heinhtetlulu14@gmail.com>
2024-02-09 12:22:21 -0800

    Implement navbar

---
+++ components/navigation/navbar.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import styles from '@/styles/navbar.module.css';
+import { FiMenu } from 'react-icons/fi';
+
+const Navbar = ({ handleToggle }) => {
+    return (
+        <div className={styles.container}>
+            <p className={styles.title}>
+                <Link href="/">Soteria</Link>
+            </p>
+
+            <ul className={styles.links}>
+                <li className={styles.link}>
+                    <Link href="/websites">websites</Link>
+                </li>
+                <li className={styles.link}>
+                    <Link href="/youtube">youtube</Link>
+                </li>
+                <li className={styles.link}>
+                    <Link href="/donations">donations</Link>
+                </li>
+                <li className={styles.link}>
+                    <Link href="/campaigns">campaigns</Link>
+                </li>
+            </ul>
+            <FiMenu className={styles.icon} onClick={handleToggle} />
+        </div>
+    );
+}
+
+export default Navbar;
