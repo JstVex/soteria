70fb5d27c2ac7b1bae897e720b4b9bbb94153337
JstVex <heinhtetlulu14@gmail.com>
2024-02-12 13:53:18 -0800

    Create overlay navigation for mobile

--- components/navigation/navbar.js
+++ components/navigation/navbar.js
@@ -1,10 +1,37 @@
+'use client'
+
 import Link from 'next/link';
 import styles from '@/styles/navbar.module.css';
+import { useEffect, useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
+import { MdOutlineClose } from 'react-icons/md';
 
-const Navbar = ({ handleToggle }) => {
+const Navbar = () => {
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    const handleMenuClose = () => {
+        setTimeout(() => {
+            setMobileMenuOpen(false);
+        }, 300);
+    };
+
+    useEffect(() => {
+        const handleBodyOverflow = () => {
+            document.body.style.overflow = isMobileMenuOpen ? "hidden" : "auto";
+        };
+
+        handleBodyOverflow();
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [isMobileMenuOpen]);
+
     return (
-        <div className={styles.container}>
+        <nav className={styles.container}>
             <p className={styles.title}>
                 <Link href="/">Soteria</Link>
             </p>
@@ -23,8 +50,27 @@ const Navbar = ({ handleToggle }) => {
                     <Link href="/campaigns">campaigns</Link>
                 </li>
             </ul>
-            <FiMenu className={styles.icon} onClick={handleToggle} />
-        </div>
+            <FiMenu className={styles.icon} onClick={handleMenuClick} />
+            <div className={`${styles.mobile_container} ${isMobileMenuOpen ? styles.mobile_on : styles.mobile_off}`}>
+                <div className={styles.mobile_wrapper}>
+                    <MdOutlineClose className={styles.mobile_close} onClick={handleMenuClick} />
+                    <ul className={styles.mobile_links}>
+                        <li className={styles.mobile_link} onClick={handleMenuClose}>
+                            <Link href="/websites">websites</Link>
+                        </li>
+                        <li className={styles.mobile_link} onClick={handleMenuClose}>
+                            <Link href="/youtube">youtube</Link>
+                        </li>
+                        <li className={styles.mobile_link} onClick={handleMenuClose}>
+                            <Link href="/donations">donations</Link>
+                        </li>
+                        <li className={styles.mobile_link} onClick={handleMenuClose}>
+                            <Link href="/campaigns">campaigns</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
